d6c91e2008b250b113545cb038b12d9a56476fde
EugenieIk <88140466+EugenieIk@users.noreply.github.com>
2021-09-02 16:28:35 +0200

    Add files via upload

--- application/Main.java
+++ application/Main.java
@@ -1,26 +1,40 @@
 package application;
-	
-import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
+import Code.Arbre;
+import Code.Noeud;
+import Code.Stagiaire;
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+//import javafx.scene.layout.BorderPane;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
 
 public class Main extends Application {
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			
+			Parent root=FXMLLoader.load(getClass().getResource("/application/ApplicationAdmin.fxml"));
+			Scene scene = new Scene(root,800,500);
+			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.setTitle("Annuaire");
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
 	
+	
 	public static void main(String[] args) {
 		launch(args);
+		
+		
 	}
 }
--- application/MainController.java
+++ application/MainController.java
@@ -1,5 +1,187 @@
 package application;
 
+import Code.Stagiaire;
+
+
+
+import java.net.URL;
+
+
+import java.util.ResourceBundle;
+
+
+
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+
 public class MainController {
 
+	@FXML
+	private TextField tfNom;
+
+	@FXML
+	private TextField tfPrenom;
+
+	@FXML
+	private TextField tfAdresse;
+
+	@FXML
+	private TextField tfMail;
+
+	@FXML
+	private TextField tfTheme;
+
+	@FXML
+	private TextField tfDebut;
+
+	@FXML
+	private TextField tfDuree;
+
+	@FXML
+	private TextField tfRecherche;
+
+	@FXML
+	private TableView<Stagiaire> tvStagiaire;
+
+	@FXML
+	private TableColumn<Stagiaire, String> colNom;
+
+	@FXML
+	private TableColumn<Stagiaire, String> colPrenom;
+
+	@FXML
+	private TableColumn<Stagiaire, String> colAdresse;
+
+	@FXML
+	private TableColumn<Stagiaire, String> colMail;
+
+	@FXML
+	private TableColumn<Stagiaire, String> colTheme;
+
+	@FXML
+	private TableColumn<Stagiaire, String> colDebut;
+
+	@FXML
+	private TableColumn<Stagiaire, String> colDuree;
+
+	@FXML
+	private Button btnAjouter;
+
+	@FXML
+	private Button btnModifier;
+
+	@FXML
+	private Button btnSuprimer;
+
+	@FXML
+	private Button btnRechercher;
+
+	@FXML
+	private Button btnImprimer;
+	
+	
+	
+	@FXML
+	private void handleButtonAction(ActionEvent event) {
+		System.out.println("test");
+
+		if (event.getSource() == btnAjouter) {
+
+			AjouterEnregistrement();
+		}
+
+		else if (event.getSource() == btnSuprimer) {
+
+			DeleteEnregistrement();
+		}
+
+		else {
+			UpdateEnregistrement();
+		}
+	}
+
+	private void AjouterEnregistrement() {
+
+	}
+
+	private void UpdateEnregistrement() {
+
+	}
+
+	private void DeleteEnregistrement() {
+
+	}
+	
+
+
+	
+	ObservableList<Stagiaire>list = FXCollections.observableArrayList(
+			
+			
+			new Stagiaire("dfdfg","dfsfs"),
+			new Stagiaire("ggggg","uuuuuuu")
+			);
+	
+	public void initialize(URL url, ResourceBundle resources) {
+		
+		colNom.setCellValueFactory(new PropertyValueFactory<Stagiaire, String>("nom"));
+		colNom.setCellValueFactory(new PropertyValueFactory<Stagiaire, String>("prenom"));
+		tvStagiaire.setItems(list);
+	}
+	
+	
+	
+
+//	@FXML
+//	public void initialize(URL location, ResourceBundle resources) {
+//		 
+//		ObservableList<Stagiaire>items = FXCollections.observableArrayList();
+//		items.add(new Stagiaire("bob", "fdfdff"));
+//		items.add(new Stagiaire("dfgfd", "zzzzz"));
+//		items.add(new Stagiaire("aaaaaaaaa", "rrrrrrr"));
+//		tvStagiaire.setItems(items);
+//		colNom.setCellValueFactory(new PropertyValueFactory<Stagiaire, String>("colNom"));
+//		colPrenom.setCellValueFactory(new PropertyValueFactory<Stagiaire, String>("colPrenom"));
+//	}
+//
+// 
 }
+
+
+//
+//public void afficherStagiaire()
+//
+//{
+//	try {
+//	ObservableList<Stagiaire> list= FXCollections.observableArrayList();	
+//	//tvStagaire.getItems().add(list);
+//	//String nomDuFichier = "Fichiertxt/annuaire.txt";
+//	//List<String> liste = new ArrayList<>();
+//	//liste = Fichier.initFichier(nomDuFichier);
+//
+//	colNom.setCellValueFactory(new PropertyValueFactory<Stagiaire, String>("Nom"));
+//	colNom.setCellValueFactory(new PropertyValueFactory<Stagiaire, String>("Prenom"));
+////	colNom.setSortType(TableColumn.SortType.DESCENDING);
+////	colPrenom.setSortable(false);
+//	
+//     table.setItems(list);
+//     
+//     table.getColumns().addAll(colNom, colPrenom);
+//	
+//
+//}
+//	catch (Exception e) {
+//		System.err.println("Error" + e);
+//	
+//}
+	//tvStagaire.getItems().add(list);}
+	
